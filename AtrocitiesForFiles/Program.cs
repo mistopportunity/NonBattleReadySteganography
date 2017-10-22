63f872c03c60bce10820937a3a7c1c86c5c82b75
Samuel Robinson <pinkstarsr11@gmail.com>
2017-10-21 21:38:50 -0700

    Initial commit

---
+++ AtrocitiesForFiles/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace AtrocitiesForFiles {
+	internal static class Program {
+		private const string ExportFolder = "Atrocities";
+		private static void Main(string[] args) {
+			Console.Title="Atrocities for files";
+			if(args==null) {
+				Console.WriteLine("No arguments (for file paths) provdied)");
+				goto End;
+			}
+			switch(args.Length) {
+				default:
+					List<string> validFilePaths = new List<string>();
+					List<string> usedFiles = new List<string>();
+					foreach(string arg in args) {
+						if(File.Exists(arg)) {
+							validFilePaths.Add(arg);
+						}
+					}
+					Directory.CreateDirectory(ExportFolder);
+					foreach(string file in validFilePaths) {
+						string name = Path.GetFileNameWithoutExtension(file);
+						string extension = Path.GetExtension(file);
+						string saveName = name;
+						int counter = 1;
+						while(usedFiles.Contains(saveName)) {
+							saveName=$"{name} {++counter}";
+						}
+						usedFiles.Add(saveName);
+						ConvertFileByBytes(file,$@"{ExportFolder}\{saveName}{extension}");
+					}
+					break;
+				case 0:
+					Console.WriteLine("An argument was provided, but it is empty.");
+					break;
+			}
+			End:
+			Console.Write($"Press any key to exit..");
+			Console.ReadKey(true);
+		}
+		private static void ConvertFileByBytes(string sourceFile,string savePath) {
+			Console.WriteLine($"Starting byte conversion for {sourceFile}");
+			byte[] bytes = File.ReadAllBytes(sourceFile);
+			int dimension = (int)Math.Ceiling(Math.Sqrt(bytes.Length));
+			int totalSize = dimension*dimension;
+			Bitmap output = new Bitmap(dimension,dimension);
+			int index = 0;
+			while(index<bytes.Length) {
+				Color color = Color.FromArgb(byte.MaxValue,bytes[index],bytes[index],bytes[index]);
+				output.SetPixel(index%dimension,index/dimension,color);
+				index+=1;
+			}
+			while(index<totalSize) {
+				output.SetPixel(index%dimension,index/dimension,Color.Transparent);
+				index+=1;
+			}
+			output.Save($"{savePath}.png",ImageFormat.Png);
+			Console.WriteLine($"Finished byte conversion for {sourceFile}");
+		}
+		private static void ConvertFileByBits(string sourceFile,string savePath) {
+			Console.WriteLine($"Starting bit conversion for {sourceFile}");
+			BitArray bits = new BitArray(File.ReadAllBytes(sourceFile));
+			int dimension = (int)Math.Ceiling(Math.Sqrt(bits.Count));
+			int totalSize = dimension*dimension;
+			Bitmap output = new Bitmap(dimension,dimension);
+			int index = 0;
+			while(index<bits.Count) {
+				Color color = bits.Get(index) ? Color.White : Color.Black;
+				output.SetPixel(index%dimension,index/dimension,color);
+				index+=1;
+			}
+			while(index<totalSize) {
+				output.SetPixel(index%dimension,index/dimension,Color.Gray);
+				index+=1;
+			}
+			output.Save($"{savePath}.png",ImageFormat.Png);
+			Console.WriteLine($"Finished bit conversion for {sourceFile}");
+		}
+	}
+}
---
+++ CrimesAgainstJavaScript/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CrimesAgainstJavaScript {
+	internal static class Program {
+		private static void Main(string[] args) {
+			if(args==null) {
+				Console.WriteLine("No arguments (for file path) provdied!");
+				goto End;
+			}
+			switch(args.Length) {
+				case 0:
+					Console.WriteLine("Please supply a file path in this program's runtime arguments!");
+					goto End;
+				case 1:
+					if(File.Exists(args[0])) {
+						Bitmap bitmap = new Bitmap(args[0]);
+						int dimension = bitmap.Width;
+						if(dimension!=bitmap.Height) {
+							Console.WriteLine("The supplied image isn't square and at the time of writing this, it is impossible for the compiler to export an image that isn't square. If something has since changed, Sam is an idiot for not having the foresight that he would inevitably change something. Although, it appears he did so he is just being lazy and negligent.");
+							goto End;
+						}
+
+
+						List<byte> bytes = new List<byte>();
+
+						int byteLength = dimension*dimension;
+						int index = 0;
+						while(index < byteLength) {
+							Color color = bitmap.GetPixel(index%dimension,index/dimension);
+							if(color.A==byte.MinValue) {
+								break;
+							}
+							bytes.Add(color.R);
+							index+=1;
+						}
+						string saveName = Path.GetFileNameWithoutExtension(args[0])+".RENAME ME";
+						File.WriteAllBytes(saveName,bytes.ToArray());
+						Console.WriteLine($"Saved {args[0]} as {saveName}");
+					} else {
+						Console.WriteLine("Somehow (because you're an idiot) this file doesn't exist!");
+					}
+					goto End;
+				default:
+					Console.WriteLine("I can only convert one file at a time!");
+					goto End;
+			}
+
+			End:
+			Console.Write("Press any key to exit..");
+			Console.ReadKey(true);
+		}
+	}
+}
